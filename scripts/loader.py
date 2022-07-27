48622d9aa0ccad3505de298c6fae70e090e27cae
sel6 <kabodshekinah@gmail.com>
2022-07-28 01:17:50 +0300

    add corrected loader file

--- scripts/loader.py
+++ scripts/loader.py
@@ -1,4 +1,3 @@
-import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine
 import pandas as pd
@@ -8,7 +7,7 @@ class Warehouse():
     def __init__(self):
         pass
         
-    def DBConnect(self, dbName=None):
+    def DBConnect(self):
         """
         A function to connect to PostgresSQL database
         """
@@ -17,18 +16,41 @@ class Warehouse():
         return p_engine
     
 
-    def createTables(self, dbName: str, path: str) -> None:
+    def createTables(self, path: str) -> None:
         """
-        A function to create SQL table
+        A function to create PostgresSQL table
         """
-        fd = open('schema.sql', 'r')
+        p_engine = DBConnect()
+        fd = open(path, 'r')
         readsqlFile = fd.read()
         fd.close()
-        p_sqlCommands = readsqlFile.split(';')
-        for command in p_sqlCommands:
+        sqlCommands = readsqlFile.split(';')
+        for command in sqlCommands:
             try:
-        result = p_engine.execute(command)
-          except Exception as e:
-            print('command skipped: ', command)
-            print(e)
-          
+                result = p_engine.execute(command)
+            except Exception as e:
+                print('command skipped: ', command)
+                print(e)
+
+    def insert_warehouse(self):
+        """
+        A function to insert values from dataframe to Postgres Database
+        """
+        p_engine = DBConnect()
+        df = pd.read_csv("extracted.csv")
+        df.drop(["Unnamed: 0"], axis=1, inplace=True)
+        for _, row in df.iterrows():
+            p_sqlQuery = """INSERT INTO elt 
+            (track_id, cars, traveled_d, avg_speed, lat, lon,
+                        speed, lon_acc, lat_acc, time)
+                    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+            data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9])
+            try:
+                p_engine.execute(p_sqlQuery, data)
+                print('Data inserted successfully')
+            except Exception as e:
+                print('Error: ', e)
+                
+
+if __name__=="__main__":
+    wr = Warehouse()
